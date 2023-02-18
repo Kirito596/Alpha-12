257da41176f7eddeace5ccc4a981507a72a1410c
Kirito596 <120283034+Kirito596@users.noreply.github.com>
2023-02-19 00:04:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const start = (bocchi = new Client()) => {
     // Creating a localhost
     app.get('/', (req, res) => res.status(200).send('Bocchi Client'))
     const PORT = process.env.PORT || 8080 || 5000 || 3000
-    app.listen(PORT, () => {
+    app.listen(PORT, () => { /home/userland/Alpha/index.js 
         console.log(color('Localhost is running!', 'yellow'))
     })  
 
